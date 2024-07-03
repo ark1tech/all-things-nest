ebb477a69ab34e1f2829bbe86a208efd6e108eea
RK Mañago <rbmanago1@up.edu.ph>
2024-07-03 14:02:54 +0800

    feat: update ninjas controller

--- mini-projects/ninja-api/src/ninjas/ninjas.controller.ts
+++ mini-projects/ninja-api/src/ninjas/ninjas.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Param, Put, Delete, Query} from '@nestjs/common';
+import * as turtleNinjas from './data.json';
 
 @Controller('ninjas')
-export class NinjasController {}
+
+// API Routes Go Here
+export class NinjasController {
+    // GET /ninjas --> Ninja[] using @Get() decorator
+    @Get()
+    getNinjas() {
+        return turtleNinjas;
+    }
+
+    // @Query('type') decorator can be used here -> ?type=[param]
+    @Get()
+    getNinjasWithQuery(@Query('type') query:string) {
+        return [query];
+    }
+
+    // GET /ninjas/:id --> Ninja with @Param() decorator
+    // :id convention?
+    // parsed as string probably so == instead of ===
+    @Get(':id')
+    getOneNinja(@Param('id') id: number) {
+        return turtleNinjas.find((ninja) => ninja.id == id);
+    }
+
+    // POST /ninjas
+    @Post()
+    createNinja() {
+        return turtleNinjas;
+    }
+
+    // PUT /ninjas/:id --> update Ninja
+    @Put(':id')
+    updateNinjas(@Param('id') id: number) {
+        return turtleNinjas.find((ninja) => ninja.id == id);
+    }   
+
+    // DELETE /ninjas/:id --> del Ninja
+    @Delete(':id')
+    deleteNinja(@Param('id') id: number) {
+        return turtleNinjas.filter((ninja) => ninja.id != id);
+    }
+}
