8707fa9095d24e9da80dc9ac16a1691ef79059ae
RK Mañago <rbmanago1@up.edu.ph>
2024-07-08 08:13:31 +0800

    fix: add booksmodule to main module

--- mini-projects/library-api/src/app.module.ts
+++ mini-projects/library-api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 // import { BooksModule } from './infrastructure/books/books.module';
 import { AuthorsModule } from './infrastructure/authors/authors.module';
+import { BooksModule } from './infrastructure/books/books.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-    imports: [AuthorsModule, DatabaseModule]
+    imports: [AuthorsModule, BooksModule, DatabaseModule]
 })
 export class AppModule {}
     
