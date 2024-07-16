f60f866e1ad99cca506ac9d036d14aaf3e52633d
RK Mañago <rbmanago1@up.edu.ph>
2024-07-16 14:40:27 +0800

    fix: remove redundant imports

--- mini-projects/library-nest-prisma/src/author-service/author.module.ts
+++ mini-projects/library-nest-prisma/src/author-service/author.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthorService } from './author-prisma.service';
 import { AuthorController } from './author.controller';
-import { PrismaModule } from '../database-service/prisma/prisma.module';
+// import { PrismaModule } from '../database-service/prisma/prisma.module';
 
 @Module({
-    imports: [PrismaModule],
+    // imports: [PrismaModule],
     controllers: [AuthorController],
     providers: [AuthorService]
 })
--- mini-projects/library-nest-prisma/src/book-service/book.module.ts
+++ mini-projects/library-nest-prisma/src/book-service/book.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BookService } from './book-prisma.service';
 import { BookController } from './book.controller';
-import { PrismaModule } from '../database-service/prisma/prisma.module';
+// import { PrismaModule } from '../database-service/prisma/prisma.module';
 
 @Module({
-    imports: [PrismaModule],
+    // imports: [PrismaModule],
     controllers: [BookController],
     providers: [BookService]
 })
